71fe35f53f9e362a88fa8d86c46ea6175ff2584f
Nikolai Kochubeev <nikolay.kochubeev@gmail.com>
2022-04-09 02:37:41 +0300

    added BankAccountValidator.cs

---
+++ Minibank.Core/Domain/BankAccounts/Validators/BankAccountValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Minibank.Core.Domain.BankAccounts.Validators
+{
+    public class BankAccountValidator : AbstractValidator<BankAccountModel>
+    {
+        public BankAccountValidator()
+        {
+            RuleFor(x => x.IsActive).Equal(true).WithMessage($"Account is not active");
+        }
+    }
+}
