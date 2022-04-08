1c6a33e7fa277331667685949e04cb5bb4845afb
Nikolai Kochubeev <nikolay.kochubeev@gmail.com>
2022-04-09 02:36:59 +0300

    added UserValidator.cs

---
+++ Minibank.Core/Domain/Users/Validators/UserValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Minibank.Core.Domain.Users.Validators
+{
+    public class UserValidator : AbstractValidator<UserModel>
+    {
+        public UserValidator()
+        {
+            RuleFor(x => x.Login).NotEmpty().WithMessage("Login cannot be empty");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email cannot be empty");
+        }
+    }
+}
